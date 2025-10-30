df46e2649c9031cc53050398106d260bb08b2d5d
Kamillendampf <raphael@hhaerle.de>
2025-10-30 15:06:35 +0100

    feat: Read PDF Files Reads a over CLI given PDF file and print the conent of it

--- core.py
+++ core.py
@@ -1,8 +1,7 @@
 import os
 from collections.abc import Sequence
 
-from PyPDF2 import PdfMerger
-
+from PyPDF2 import PdfMerger, PdfReader
 
 
 def merge_pdf(output_path: str, input_paths: Sequence[str]) -> None:
@@ -36,3 +35,36 @@ def merge_pdf(output_path: str, input_paths: Sequence[str]) -> None:
         merger.close()
     except TypeError:
         raise f"Something went wrong"
+
+def read_pdf(input_paths: str, selected_pages: list[int], password : str) -> str:
+    if not input_paths:
+        raise ValueError("Input paths cannot be empty")
+
+    abs_input_path = os.path.abspath(input_paths)
+
+    reader = PdfReader(abs_input_path)
+
+    if reader.is_encrypted:
+        if password:
+            raise ValueError("PDF is encrypted. Password required!")
+        if reader.decrypt(password) == 0:
+            raise ValueError("Wrong PDF password!")
+
+    pdf_pages = reader.pages
+
+    if len(pdf_pages) == 0:
+        raise ValueError("PDF is empty")
+
+    pdf_parts = []
+    if selected_pages is None:
+        for page in pdf_pages:
+            pdf_parts.append(page.extract_text() or "")
+    else:
+        total_pages = len(pdf_pages)
+        bad = [ page for page in selected_pages if page < 0 or page >= total_pages ]
+        if bad:
+            raise ValueError(f'Invalide PDF pages: {bad} (o...{total_pages-1}')
+        for i in selected_pages:
+            pdf_parts.append(reader.pages[i].extract_text() or "")
+
+    return "\n".join(pdf_parts)
--- main.py
+++ main.py
@@ -18,14 +18,16 @@ def build_args_parser() -> argparse.ArgumentParser:
 
     sub_parser = ap.add_subparsers(dest="cmd", required =False,)
 
-    sp_m = sub_parser.add_parser('merge', help = 'Start PDF merge')
-    sp_m.add_argument("-m", "--merge",
-                      dest = 'merge_pdf',
-                      action='store_true',
-                      help = 'Merge PDF Files in given order.')
+    sp_m = sub_parser.add_parser('merge', help = 'Merges two or more PDF files in to one PDF')
     sp_m.add_argument("-o", "--output", nargs = '?', help = 'Specify output file.')
     sp_m.add_argument("-i", "--input", nargs = '*', help = 'Specify input files (defines the order in which the files are merged)')
     sp_m.set_defaults(func=handle_merge)
+
+    sp_r = sub_parser.add_parser('read', help = 'Reads a PDF File \n -f --file spezifies the input file \n -p --page Specify sides that should be read \n -pass --password For decryption of PDF file if it`s secured')
+    sp_r.add_argument("-f", "--file", nargs = '?', help = 'Specify file to read')
+    sp_r.add_argument("-p", "--pages", help = 'Specify sides that should be read')
+    sp_r.add_argument("-pass", "--password", help = 'For decryption of PDF file if it`s secured')
+    sp_r.set_defaults(func=handle_reader)
 
     return ap
 
@@ -37,6 +39,11 @@ def handle_merge(args: argparse.Namespace) -> int:
     except Exception as e:
         print(f"Error: {e}", file = sys.stderr)
         return 1
+
+def handle_reader(args: argparse.Namespace) -> int:
+    read_file= core.read_pdf(args.file, args.pages, args.password)
+    print(read_file)
+    return 0
 
 
 if __name__ == '__main__':
