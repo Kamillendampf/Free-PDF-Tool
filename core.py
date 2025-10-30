697580c9f290d12c50873ff85ffc6e2c4485a924
Kamillendampf <raphael@hhaerle.de>
2025-10-30 15:24:20 +0100

    refactor(core) add type specifications and remove code Add type specifications for variables removes code that wasen`t used

--- core.py
+++ core.py
@@ -5,27 +5,20 @@ from PyPDF2 import PdfMerger, PdfReader
 
 
 def merge_pdf(output_path: str, input_paths: Sequence[str]) -> None:
+
     if not input_paths:
         raise ValueError("Input paths cannot be empty")
 
-    abs_output_path = os.path.abspath(output_path)
-    abs_input_path = [os.path.abspath(p) for p in input_paths]
+    abs_output_path : str = os.path.abspath(output_path)
+    abs_input_path : list[str] = [os.path.abspath(p) for p in input_paths]
     if abs_output_path in abs_input_path:
         raise ValueError("Output paths cannot be the same as input paths")
 
-    missing = [p for p in input_paths if not os.path.isfile(p)]
+    missing : list[str] = [p for p in input_paths if not os.path.isfile(p)]
     if missing:
         raise FileNotFoundError(f'File Not Found: {', '.join(missing)}')
 
-    print("PFAD")
-    print(abs_output_path)
-
-    if len(abs_output_path) != 0:
-        out_dir = os.path.dirname(abs_output_path)
-        print(out_dir)
-    else:
-        out_dir = "./mergedFiles.pdf"
-    merger = PdfMerger()
+    merger : PdfMerger = PdfMerger()
 
     try:
         for input_path in input_paths:
@@ -36,13 +29,18 @@ def merge_pdf(output_path: str, input_paths: Sequence[str]) -> None:
     except TypeError:
         raise f"Something went wrong"
 
-def read_pdf(input_paths: str, selected_pages: list[int], password : str) -> str:
+def read_pdf(input_paths: str, raw_selected_pages: list[str], password : str) -> str:
+    try:
+        selected_pages : list[int] = [int(raw_selected_page) for selected_page in raw_selected_pages ]
+    except ValueError:
+        raise ValueError("Selected pages must be a positive integer")
+
     if not input_paths:
         raise ValueError("Input paths cannot be empty")
 
-    abs_input_path = os.path.abspath(input_paths)
+    abs_input_path : str = os.path.abspath(input_paths)
 
-    reader = PdfReader(abs_input_path)
+    reader : PdfReader = PdfReader(abs_input_path)
 
     if reader.is_encrypted:
         if password:
@@ -50,7 +48,7 @@ def read_pdf(input_paths: str, selected_pages: list[int], password : str) -> str
         if reader.decrypt(password) == 0:
             raise ValueError("Wrong PDF password!")
 
-    pdf_pages = reader.pages
+    pdf_pages : reader.pages = reader.pages
 
     if len(pdf_pages) == 0:
         raise ValueError("PDF is empty")
